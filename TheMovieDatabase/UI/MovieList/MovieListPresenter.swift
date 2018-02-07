326927ff636153deb6d834471c851694e28d9156
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2018-02-07 14:08:34 -0400

    movie list contracts

---
+++ TheMovieDatabase/UI/MovieList/MovieListPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListPresenter.swift
+//  TheMovieDatabase
+//
+//  Created by Ilbert Esculpi on 2/7/18.
+//  Copyright © 2018 Ilbert Esculpi. All rights reserved.
+//
+
+import UIKit
+
+class MovieListPresenter: NSObject {
+
+}
