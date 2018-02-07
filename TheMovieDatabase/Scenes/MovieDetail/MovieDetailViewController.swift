bf4c60adc1cc83b79470a9a0ece01da7d02c4c87
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2018-02-07 12:37:59 -0400

    add app basic scenes: Home / MovieList / MovieDetail

---
+++ TheMovieDatabase/Scenes/MovieDetail/MovieDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Ilbert Esculpi on 2/7/18.
+//  Copyright © 2018 Ilbert Esculpi. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+
+}
---
+++ TheMovieDatabase/Scenes/MovieList/MovieListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Ilbert Esculpi on 2/7/18.
+//  Copyright © 2018 Ilbert Esculpi. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+
+}
