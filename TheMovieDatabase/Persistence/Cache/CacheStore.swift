ff011e610f8dff11ac7e4dcbea02b469532096d2
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2018-02-11 08:49:02 -0400

    cache network response

---
+++ TheMovieDatabase/Persistence/Cache/CacheStore.swift
@@ -0,0 +1,13 @@
+//
+//  CacheStore.swift
+//  TheMovieDatabase
+//
+//  Created by Ilbert Esculpi on 2/10/18.
+//  Copyright © 2018 Ilbert Esculpi. All rights reserved.
+//
+
+import UIKit
+
+class CacheStore: NSObject {
+
+}
---
+++ TheMovieDatabase/Persistence/Contracts/CacheContract.swift
@@ -0,0 +1,9 @@
+//
+//  CacheContracts.swift
+//  TheMovieDatabase
+//
+//  Created by Ilbert Esculpi on 2/10/18.
+//  Copyright © 2018 Ilbert Esculpi. All rights reserved.
+//
+
+import Foundation
